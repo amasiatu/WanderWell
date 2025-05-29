3274b8ce6419d8e339d0f184b778778b322e9a35
CHINYERE AMASIATU <amasiatu@umich.edu>
2025-05-28 22:57:32 -0400

    set up frontend and backend

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,10 +1,25 @@
 from flask import Flask, jsonify, render_template, request, redirect, url_for
+from flask_cors import CORS
 import json
 import os
 
 app = Flask(__name__)
+CORS(app)
 
 # Load country data from JSON file
-with open('data/countries.json') as f:
-    countries = json.load(f)
+# with open('data/countries.json') as f:
+#     countries = json.load(f)
+@app.route('/')
+@app.route("/home")
+def home():
+    return {
+        'Name':"Asha", 
+        "Age":"20",
+        "Date":'5/28/25', 
+        "Major":"History",
+        "Travel Habits": "Often, 2-3 times a year"
+        }
+
+if __name__ == '__main__':
+    app.run(debug=True, host="0.0.0.0")
     
